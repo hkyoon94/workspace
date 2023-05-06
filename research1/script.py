c6a24b8750ecb96acd9a7357376b8e41be6943da
hkyoon94 <honggyu@pozalabs.com>
2023-05-06 19:26:29 +0900

    추가

--- research1/script.py
+++ research1/script.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from test_pkg.pkg1.module import add
+from test_pkg.pkg2.module import mul
 
 module_path = os.path.abspath(os.path.join(".."))
 
@@ -9,3 +10,4 @@ if module_path not in sys.path:
     sys.path.append(module_path)
 
 add(2, 3)
+mul(3, 4)
