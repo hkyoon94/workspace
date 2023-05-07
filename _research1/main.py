3ccb6e3c394b68f1142f935330f00809426f6eca
hkyoon94 <honggyu@pozalabs.com>
2023-05-07 13:26:45 +0900

    module path

--- _research1/main.py
+++ _research1/main.py
@@ -1,13 +1,10 @@
 import os
 import sys
 
+sys.path.append(os.path.abspath("/home/honggyu/workspace"))  # noqa: E402
+
 from test_pkg.pkg1.module import add
 from test_pkg.pkg2.module import mul
-
-module_path = os.path.abspath(os.path.join(".."))
-
-if module_path not in sys.path:
-    sys.path.append(module_path)
 
 add(2, 3)
 mul(3, 4)
